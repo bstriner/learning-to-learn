febee8e6a27f6fda4c49553afa3791ffda6c1145
bstriner <bstriner@yahoo.com>
2017-02-04 12:45:31 -0500

    Add MNIST script

--- experiments/mnist_sgd_discrete_dqn.py
+++ experiments/mnist_sgd_discrete_dqn.py
@@ -1,10 +1,24 @@
+import argparse
+import numpy as np
+import gym
+from gym import wrappers
+import pandas as pd
 import sys
-
-from keras.layers import Dense, Flatten, Dropout, LeakyReLU
+import gym_learning_to_learn
+import pandas as pd
+from gym_learning_to_learn.wrappers import DataRecorder
 from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Dropout, LeakyReLU
 from keras.regularizers import l1l2
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+import os
 from learning_to_learn import dqn
-ENV_NAME = 'SGD-Polynomial-Discrete-v0'
+
+ENV_NAME = 'SGD-MNIST-Discrete-v0'
 
 
 def create_model(window_length, env):
@@ -28,7 +42,7 @@ def create_model(window_length, env):
 
 def main(argv):
     dqn.main(argv,
-             "output/polynomial-sgd-discrete/dqn.h5",
+             "output/mnist-sgd-discrete/dqn.h5",
              ENV_NAME,
              create_model)
 
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='learning_to_learn',
+      version='0.0.1',
+      install_requires=['gym'],
+      author='Ben Striner',
+      url='https://github.com/bstriner/learning-to-learn',
+      packages=find_packages())
